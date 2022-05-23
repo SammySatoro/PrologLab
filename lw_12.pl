5af8c505d7e71ba7421ba145b27b5da07eba0f88
SammySatoro <80969335+SammySatoro@users.noreply.github.com>
2022-05-24 02:35:50 +0300

    Update lw_12.pl

--- lw_12.pl
+++ lw_12.pl
@@ -1,17 +1,17 @@
 % Найти количество делителей числа, не делящихся на 3
 
-countDivs(N, N, 0):-!.
-countDivs(N, Div, Count):-
+countDivsUp(N, N, 0):-!.
+countDivsUp(N, Div, Count):-
 	NextDiv is Div + 1,
 	0 is N mod NextDiv, 0 =\= NextDiv mod 3, 	 
-	countDivs(N, NextDiv, NextCount),
+	countDivsUp(N, NextDiv, NextCount),
 	Count is NextCount + 1,!;
 	NextDiv is Div + 1,
 	0 is N mod NextDiv, 0 is NextDiv mod 3, 	 
-	countDivs(N, NextDiv, NextCount),
+	countDivsUp(N, NextDiv, NextCount),
 	Count is NextCount,!;
 	NextDiv is Div + 1,
 	0 =\= N mod NextDiv,	 
-	countDivs(N, NextDiv, NextCount),
+	countDivsUp(N, NextDiv, NextCount),
 	Count is NextCount,!.
-countDivs(N, Count):- countDivs(N, 0, Count).
+countDivsUp(N, Count):- countDivsUp(N, 0, Count).
