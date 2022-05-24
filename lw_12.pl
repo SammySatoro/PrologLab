5b6eeac394e739b70e07c7d87b2de106114f8793
SammySatoro <80969335+SammySatoro@users.noreply.github.com>
2022-05-24 17:39:20 +0300

    Update lw_12.pl

--- lw_12.pl
+++ lw_12.pl
@@ -66,3 +66,43 @@ sumOfDivs(N, Div, Summutor, Sum):-
 	sumOfDivs(N, NextDiv, NextSummutor, Sum), !;
 	NextDiv is Div + 1,
 	sumOfDivs(N, NextDiv, Summutor, Sum), !.
+
+/* 13
+Удивительно, но есть только три числа, которые можно записать как сумму
+четвертых степеней их цифр:
+1634 = 1 4 + 6 4 + 3 4 + 4 4
+8208 = 8 4 + 2 4 + 0 4 + 8 4
+9474 = 9 4 + 4 4 + 7 4 + 4 4
+Так как 1 = 1 4 не сумма, она не включена.
+Сумма этих чисел составляет 1634 + 8208 + 9474 = 19316
+Найдите сумму всех чисел, которые можно записать как сумму пятых
+степеней их цифр.
+Задача должна быть решена без использования списков.
+*/
+
+myPow(_, 0, 1):-!.
+myPow(N, P, Res):-
+	NextP is P - 1,
+	myPow(N, NextP, NextRes),
+	Res is NextRes * N.
+
+
+isSumOfPows(N):- isSumOfPows(N, N, 0).	
+isSumOfPows(N, 0, Sum):- N is Sum, !.
+isSumOfPows(N, N2, Sum):-
+	N2 > 0,
+	NextN2 is N2 div 10,
+	Num is N2 mod 10,
+	myPow(Num, 5, Res),
+	NextSum is Sum + Res,
+	isSumOfPows(N, NextN2, NextSum),!.
+	
+sumOfSumsOfPows(N, Sum):- sumOfSumsOfPows(N, 0, Sum).
+sumOfSumsOfPows(0, Sum, Sum):-!.
+sumOfSumsOfPows(N, Summator, Sum):-
+	NextN is N - 1,
+	isSumOfPows(N),
+	NextSummator is Summator + N,
+	sumOfSumsOfPows(NextN, NextSummator, Sum),!;
+	NextN is N - 1,
+	sumOfSumsOfPows(NextN, Summator, Sum),!.
