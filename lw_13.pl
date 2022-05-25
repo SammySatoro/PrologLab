3eb4974867608eb602d49089eb95218f9046333c
SammySatoro <80969335+SammySatoro@users.noreply.github.com>
2022-05-25 21:47:29 +0300

    Update lw_13.pl

--- lw_13.pl
+++ lw_13.pl
@@ -1,5 +1,6 @@
 /* 11
-
+Дан целочисленный массив. Найти среднее арифметическое моду-
+лей его элементов.
 */
 abs1(X, AbsX):-
     X >= 0, AbsX is X,!;
@@ -14,3 +15,53 @@ sumOfAbs([Head|Tail], Count, Sum, Res):-
     abs(Head, AbsHead),
     NextSum is Sum + AbsHead,
     sumOfAbs(Tail, NextCount, NextSum, Res).
+    
+/*12
+Для введенного списка положительных чисел построить список всех по-
+ложительных простых делителей элементов списка без повторений.
+*/
+
+divCount(N, Count):- divCount(N, 0, Count).
+divCount(N, N, 0):-!.
+divCount(N, Div, Count):-
+	NextDiv is Div + 1,
+	0 is N mod NextDiv, 	 
+	divCount(N, NextDiv, NextCount),
+	Count is NextCount + 1,!;	
+	NextDiv is Div + 1,
+	0 =\= N mod NextDiv,	 
+	divCount(N, NextDiv, NextCount),
+	Count is NextCount,!.
+
+isPrime(N):- divCount(N, Count), Count is 2,!.
+
+member([H|_], H).
+member([_|T], H):- member(T, H),!. 
+
+listOfPrimeDivs(N, Divs):- listOfPrimeDivs(N, 1, [], Divs),!.
+listOfPrimeDivs(N, N1, Divs, Divs):- N1 is N + 1,!.
+listOfPrimeDivs(N, Div, Buffer, Divs):-	
+	NextDiv is Div + 1,
+	0 is N mod Div, isPrime(Div),	
+	listOfPrimeDivs(N, NextDiv, [Div|Buffer], Divs),!;
+	NextDiv is Div + 1,
+	listOfPrimeDivs(N, NextDiv, Buffer, Divs),!.
+
+listOfAllPrimeDivs(List, Divs):- listOfAllPrimeDivs(List, [], Divs),!.
+listOfAllPrimeDivs([], Divs, Divs):-!.
+listOfAllPrimeDivs([Head|Tail], Buffer, Divs):-
+	listOfPrimeDivs(Head, Primes),
+	append(Buffer, Primes, NextBuffer),
+	listOfAllPrimeDivs(Tail, NextBuffer, Divs).
+
+listOfDistinctPrimes(List, Distincts):- 
+	listOfAllPrimeDivs(List, PrimeDivs),
+	write(PrimeDivs), nl,
+	listOfDistinctPrimes(PrimeDivs, [], Distincts),!.
+	
+listOfDistinctPrimes([], Distincts, Distincts):- write("check"   | Distincts), nl,!.
+listOfDistinctPrimes([Head|Tail], Buffer, Distincts):-
+	member(Buffer, Head),
+	listOfDistinctPrimes(Tail, Buffer, Distincts);
+	appendList(Buffer, [Head], NextBuffer),	
+	listOfDistinctPrimes(Tail, NextBuffer, Distincts).
