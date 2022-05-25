5ae583b0edef0900d1fcf87b79a825e32ff90c58
SammySatoro <80969335+SammySatoro@users.noreply.github.com>
2022-05-25 09:32:27 +0300

    Update lw_13.pl

--- lw_13.pl
+++ lw_13.pl
@@ -1,1 +1,16 @@
+/* 11
 
+*/
+abs1(X, AbsX):-
+    X >= 0, AbsX is X,!;
+    X < 0, AbsX is -X,!.
+
+
+sumOfAbs([], Res):- Res is 0,!.
+sumOfAbs(List, Res):- sumOfAbs(List, 0, 0, Res),!.
+sumOfAbs([], Count, Sum, Res):- Res is Sum / Count.
+sumOfAbs([Head|Tail], Count, Sum, Res):-
+    NextCount is Count + 1,
+    abs(Head, AbsHead),
+    NextSum is Sum + AbsHead,
+    sumOfAbs(Tail, NextCount, NextSum, Res).
