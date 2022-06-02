37b39c3656a203546c3b8e77e2021d684d333b19
SammySatoro <80969335+SammySatoro@users.noreply.github.com>
2022-06-02 04:50:52 +0300

    Update lw_14.pl

--- lw_14.pl
+++ lw_14.pl
@@ -35,3 +35,101 @@ listLength([_|T],I):-listLength(T,I1),I is I1 + 1.
 
 %1.1 Дана строка. Вывести ее три раза через запятую и показать количество символов в ней.
 task11:-readS(Str,N,0),writeS(Str),write(", "),writeS(Str),write(", "),writeS(Str),write(" , "),write(N).
+
+%1.2 Дана строка. Найти количество слов.
+countOfWords([],Count):-Count is 1,!.
+countOfWords([H|T],Count):-
+	(
+		H is 32,
+		
+		countOfWords(T,Count1),
+		Count is Count1+1;
+
+		countOfWords(T,Count)
+	),!.
+
+task12:-readS(Str,N,0),countOfWords(Str,Count),write(Count).
+%1.3 Дана строка, определить самое частое слово
+getListOfWordsFromStr(Str,List):-getListOfWordsFromStr(Str,List,[]).
+getListOfWordsFromStr([],WordsList,NowWord):-append([NowWord], [],WordsList).
+getListOfWordsFromStr([H|T],WordsList,NowWord):-
+	(
+		not(H is 32),
+		
+		append(NowWord,[H],NowWord1),
+		getListOfWordsFromStr(T,WordsList,NowWord1);
+
+		getListOfWordsFromStr(T,WordsList1,[]),
+		append([NowWord],WordsList1,WordsList)
+	),!.
+
+mostRatedWordFromList(WordsList,Word):-mostRatedWordFromList(WordsList,Word,[],-1,WordsList).
+mostRatedWordFromList([],Word,NowWord,Count,StartList):-append([],NowWord,Word),!.
+mostRatedWordFromList([WordsListH|WordsListT],Word,NowWord,Count,StartList):-
+	countOfThisWordInList(StartList,WordsListH,NowCount),
+	(
+		NowCount>Count,
+
+		mostRatedWordFromList(WordsListT,Word,WordsListH,NowCount,StartList);
+
+		mostRatedWordFromList(WordsListT,Word,NowWord,Count,StartList)
+	),!.
+countOfThisWordInList([],Word,NowCount):- NowCount is 0,!.
+countOfThisWordInList([StartListH|StartListT],Word,NowCount):-
+	StartListH = Word,
+
+	countOfThisWordInList(StartListT,Word,NowCount1),
+	NowCount is NowCount1+1,!;
+
+	countOfThisWordInList(StartListT,Word,NowCount),!.
+
+mostRatedWordFromStr(Str,Word):-
+	getListOfWordsFromStr(Str,WordsList),
+	mostRatedWordFromList(WordsList,Word).
+
+task13:-readS(Str,N,0),mostRatedWordFromStr(Str,Word),writeS(Word).
+%1.4 Дана строка. Вывести первые три символа и последний три символа, если длина строки больше 5, иначе вывести первый символ столько раз, какова длина строки.
+writeFirstAndLastThree([Fchar|StrT]):- writeFirstAndLastThree([Fchar|StrT],3),!.
+writeFirstAndLastThree([Fchar|StrT],N):-
+	N>0,
+	cutLastChar(StrT,NewStr,Lchar),
+	write(Fchar),
+	N1 is N-1,
+	writeFirstAndLastThree(NewStr, N1),
+	write(Lchar),!;
+	1 is 1,!.
+
+cutLastChar([Lchar],NewStr,Lchar):-makeEmptyList(NewStr),!.
+cutLastChar([StrH|StrT],NewStr,Lchar):-
+	cutLastChar(StrT,NewStr1,Lchar),
+	append([StrH],NewStr1,NewStr),!.
+
+writeNTimesFirst([StrH|StrT],N):-
+	N>0,
+	write(StrH),
+	N1 is N-1,
+	writeNTimesFirst([StrH|StrT],N1),!;
+	1 is 1,!.
+
+task14:-
+	readS(Str,N,0),
+	(
+		N>5,
+
+		writeFirstAndLastThree(Str),!;
+		writeNTimesFirst(Str,N)
+	),!.
+%1.5 Дана строка. Показать номера символов, совпадающих с последним символом строки.
+getNumberOfCharsThatLikeLast(Str,ListNumbers):-cutLastChar(Str,_,LastChar),getNumberOfCharsLikeThat(Str,ListNumbers,LastChar),!.
+getNumberOfCharsLikeThat(Str,ListNumbers,LastChar):-getNumberOfCharsLikeThat(Str,ListNumbers,LastChar,0),!.
+getNumberOfCharsLikeThat([],ListNumbers,LastChar,I):-makeEmptyList(ListNumbers),!.
+getNumberOfCharsLikeThat([StrH|StrT],ListNumbers,LastChar,I):-
+	I1 is I+1,
+	(
+	StrH = LastChar,
+
+	getNumberOfCharsLikeThat(StrT,ListNumbers1,LastChar,I1),
+	append([I1],ListNumbers1,ListNumbers),!;
+
+	getNumberOfCharsLikeThat(StrT,ListNumbers,LastChar,I1),!	
+	).
